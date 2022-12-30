a6894e4ce06c30997072be8a754a28dc262a710e
Kelvin Rodrigues <81050267+LunarioTK@users.noreply.github.com>
2022-12-30 03:41:31 +0000

    Product page working

--- lib/Pages/product_page.dart
+++ lib/Pages/product_page.dart
@@ -12,23 +12,86 @@ class ProductPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(5, 30, 5, 5),
+      body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(produto.imgUrl),
             Stack(
               children: [
                 Container(
+                  height: 450,
+                  width: 400,
                   decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(20))),
+                  child: Image.network(
+                    produto.imgUrl,
+                    fit: BoxFit.fill,
                   ),
-                  child: IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    onPressed: Navigator.of(context).pop,
-                  ),
-                )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(15),
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.white),
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: Navigator.of(context).pop,
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 15, 15, 15),
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.white),
+                          child: IconButton(
+                            icon: const Icon(Icons.favorite_outline),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 15, 15, 15),
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.white),
+                          child: IconButton(
+                            icon: const Icon(Icons.share),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Name
+                  Text(
+                    produto.name,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  // Price
+                  Text(
+                    '${produto.price}€',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
